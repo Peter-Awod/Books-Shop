ea15eb22a94294084a55c5a548e190a33b5ec36d
Peter-Awod <peterawod74@gmail.com>
2024-08-02 10:48:25 +0300

    finish styles file

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
 const Color kPrimaryColor = Color(0xff100B20);
-const Duration kTransitionDuration=Duration(milliseconds: 250);
+const Duration kTransitionDuration = Duration(milliseconds: 250);
+const String kHanuman = 'Hanuman';
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/assets_images.dart';
 import '../../../../../core/utils/styles.dart';
 import 'custom_appbar.dart';
 import 'featured_books_list_view.dart';
@@ -10,7 +11,11 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsetsDirectional.only(start: 20,top: 20,bottom: 20,),
+      padding: EdgeInsetsDirectional.only(
+        start: 20,
+        top: 20,
+        bottom: 20,
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -21,8 +26,41 @@ class HomeViewBody extends StatelessWidget {
           ),
           Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
           ),
+          SizedBox(height: 20,),
+          BestSellerItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerItem extends StatelessWidget {
+  const BestSellerItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  image: AssetImage(
+                    AssetsImages.testImage,
+                  ),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 10,),
+          const Column(children: [],)
         ],
       ),
     );
