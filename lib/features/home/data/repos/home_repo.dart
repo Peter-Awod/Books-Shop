bab3ddc3250f55233185ed2c01923a2daf4e8792
Peter-Awod <peterawod74@gmail.com>
2024-08-11 13:31:24 +0300

    create home repo and failure class

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -5,6 +5,6 @@ import '../../../../core/errors/failures.dart';
 import '../models/book_model/book_model.dart';
 
 abstract class HomeRepo{
-  Future<Either<Failure,BookModel>> fetchFeaturedBooks();
+  Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
   Future<Either<Failure,BookModel>> fetchBestSellerBooks();
 }
