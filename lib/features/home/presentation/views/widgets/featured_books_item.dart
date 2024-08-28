502551a81b7a776bc14101753c213eb3fb58242e
Peter-Awod <peterawod74@gmail.com>
2024-08-29 01:50:29 +0300

    add cached network image

--- lib/features/home/presentation/views/widgets/featured_books_item.dart
+++ lib/features/home/presentation/views/widgets/featured_books_item.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedBooksItem extends StatelessWidget {
@@ -7,14 +8,15 @@ class FeaturedBooksItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image: DecorationImage(
-            image: NetworkImage(imageUrl),
-            fit: BoxFit.fill,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) => const Icon(
+            Icons.error_outline_outlined,
           ),
         ),
       ),
