27be0d6e05fe86e6fca9af076014bc4f9e6e0263
Peter-Awod <peterawod74@gmail.com>
2024-09-06 09:44:09 +0300

    add search end point

--- lib/core/utils/services/api_service.dart
+++ lib/core/utils/services/api_service.dart
@@ -5,8 +5,9 @@ class ApiService{
   final Dio _dio;
 
   ApiService(this._dio);
-  Future <Map<String,dynamic>> get({required String endPoint})async {
-    var response=await _dio.get('$_baseUrl$endPoint');
+  Future <Map<String,dynamic>> get({required String endPoint,String? searchString})async {
+   searchString ??= '';
+    var response=await _dio.get('$_baseUrl$endPoint$searchString');
     return response.data;
   }
 }
--- lib/core/utils/services/end_points.dart
+++ lib/core/utils/services/end_points.dart
@@ -1,6 +1,10 @@
 const String kGetBooksEndPoint =
     'volumes?Filtering=free-ebooks&Sorting=newest&q=computer science';
+
 const String kGetFeaturedBooksEndPoint =
     'volumes?Filtering=free-ebooks&q=subject:programming';
+
 const String kGetSimilarBooksEndPoint =
     'volumes?Filtering=free-ebooks&Sorting=relevance&q=computer science';
+
+const String kSearchEndPoint = 'volumes?Filtering=free-ebooks&q=subject:';
